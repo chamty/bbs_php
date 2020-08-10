b116a3c292d0d11ce390d590e69138893063969d
Chamty <ak.cha07.mt@gmail.com>
2020-08-10 23:54:45 +0900

    0810_2

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
+  $_SESSION['time'] = time();
+
+  $members = $db->prepare('SELECT * FROM members WHERE id=?');
+  $members->execute(array($_SESSION['id']));
+  $member = $members->fetch();
+} else {
+  header('Location: login.php');
+  exit();
+}
+
+if (!empty($_POST)) {
+  if ($_POST['message'] !== '') {
+    $message = $db->prepare('INSERT INTO posts SET member_id=?, reply_message_id=0, message=?, created=NOW()');
+    $message->execute(array(
+      $member['id'],
+      $_POST['message']
+    ));
+
+    header('Location: index.php');
+    exit();
+  }
+}
+
+$posts = $db->query('SELECT m.name, p.* FROM members m, posts p WHERE m.id=member_id ORDER BY p.created DESC');
+
+if (isset($_REQUEST['res'])) {
+  $response = $db->prepare('SELECT m.name, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
+  $response->execute(array($_REQUEST['res']));
+
+  $table = $response->fetch();
+  $message = '@' . $table['name'] . ' ' . $table['message'];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,27 +47,30 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <div id="app">
   <div id="container">
     <h1 id="appTitle">ひとこと掲示板</h1>
-    <div id="inputBox">
-      <div id="inputMessage">
-        <p>メッセージ<span class="error">※メッセージを入力してください</span></p>
-        <textarea cols="30" rows="10"></textarea>
+    <form action="" method="post">
+      <div id="inputBox">
+        <p><?php print(htmlspecialchars($member['name'], ENT_QUOTES)); ?>さん</p>
+        <div id="inputMessage">
+          <textarea name="message" cols="30" rows="10"><?php print(htmlspecialchars($message, ENT_QUOTES)); ?></textarea>
+        </div>
+        <button id="inputSubmit">書き込む</button>
       </div>
-      <button id="inputSubmit">書き込む</button>
-    </div>
+    </form>
     <hr width="100%">
     <div id="tweets">
+      <?php foreach($posts as $post): ?>
       <div class="tweetBox">
-        <div class="tweetUser">
-          <p class="tweetName">ユーザー名</p>
-          <p class="tweetTime">時間</p>
+        <div class="tweetInfo">
+          <p class="tweetName"><?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?></p>
+          <p class="tweetTime"><?php print(htmlspecialchars($post['created'],ENT_QUOTES)); ?></p>
+          <p class="reply"><a href="index.php?res=<?php print(htmlspecialchars($post['id'],ENT_QUOTES)); ?>">[ Re ]</a></p>
         </div>
-        <p class="tweet"><?php print(htmlspecialchars($_POST['message'], ENT_QUOTES)); ?></p>
+        <p class="tweet"><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?></p>
       </div>
+      <?php endforeach; ?>
     </div>
-  </div>
   </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,3 +1,41 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if ($_COOKIE['email'] !== '') {
+  $email = $_COOKIE['email'];
+}
+
+if (!empty($_POST)) {
+  $email = $_POST['email'];
+
+  if ($_POST['email'] !== '' && $_POST['password'] !== '') {
+    $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
+    $login->execute(array(
+      $_POST['email'],
+      sha1($_POST['password'])
+    ));
+    $member = $login->fetch();
+
+    if ($member) {
+      $_SESSION['id'] = $member['id'];
+      $_SESSION['time'] = time();
+
+      if ($_POST['save'] === 'on') {
+        setcookie('email', $_POST['email'], time()+60*60*24*14);
+      }
+
+      header('Location: index.php');
+      exit();
+    } else {
+      $error['login'] = "failed";
+    }
+  } else {
+    $error['login'] = 'blank';
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -12,26 +50,22 @@
     <h1 id="appTitle">掲示板</h1>
     <form action="" method="post">
     <dl id="inputItems">
+      <?php if ($error['login'] === 'blank'): ?>
+        <p class="error">※メールアドレスとパスワードを入力してください</p>
+      <?php endif; ?>
+      <?php if ($error['login'] === 'failed'): ?>
+        <p class="error">※メールアドレスまたはパスワードが異なっています</p>
+      <?php endif; ?>
       <dt>メールアドレス</dt>
       <dd>
-        <input type="text" name="email" value="<?php print(htmlspecialchars($_POST['email'], ENT_QUOTES)); ?>">
+        <input type="text" name="email" value="<?php print(htmlspecialchars($email, ENT_QUOTES)); ?>">
       </dd>
-      <?php if ($error['email'] === 'blank'): ?>
-        <p class="error">※メールアドレスを入力してください</p>
-      <?php endif; ?>
-      <?php if ($error['email'] === 'duplicate'): ?>
-        <p class="error">※指定されたメールアドレスは、既に登録されています</p>
-      <?php endif; ?>
       <dt>パスワード</dt>
       <dd>
         <input type="text" name="password" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>">
       </dd>
-      <?php if ($error['password'] === 'length'): ?>
-        <p class="error">※パスワードは4文字以上で入力してください</p>
-      <?php endif; ?>
-      <?php if ($error['password'] === 'blank'): ?>
-        <p class="error">※パスワードを入力してください</p>
-      <?php endif; ?>
+      <input type="checkbox" name="save" value="on">
+      <label for="save">メールアドレスを記憶する</label>
     </dl>
     <div><input type="submit" id="inputSubmit" value="ログイン"> | <a href="join/index.php">新規登録はこちら</a></div>
     </form>
